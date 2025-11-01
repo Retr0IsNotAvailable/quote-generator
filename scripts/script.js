88b1a92434dc0c323c210a455af0d9fcc7b63569
Retr0IsNotAvailable <josephmagodks15846@gmail.com>
2025-11-02 00:16:06 +0100

    add post button functionality to share quotes on X

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,7 @@ const api_url = 'https://api.quotable.io/random';
 
 const quoteElement = document.querySelector('.js-quote');
 const authorElement = document.querySelector('.js-author');
+const postButton = document.querySelector('.js-post-button');
 
 
 async function getQuote(url) {
@@ -13,6 +14,14 @@ async function getQuote(url) {
 }
 
 getQuote(api_url);
+
+function post() {
+  window.open('https://twitter.com/intent/tweet?text="' + quoteElement.innerHTML +
+    '"  --' + authorElement.innerHTML,
+    'Post Window', 'width=600px, height=300px');
+}
+
+postButton.addEventListener('click', () => post());
 
 document.querySelector('.js-new-quote')
   .addEventListener('click', () => getQuote(api_url));
